5b58459384647a2ec96406d7820d9a79fcf74ce5
Ridam26 <86104688+Ridam26@users.noreply.github.com>
2023-08-03 22:14:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "Hello ALL my jenkins project!"
+	return "Hello ALL!"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
